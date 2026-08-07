eeb269880498ffe3759f758ef63767e475dad315
aasith32-arv <aasith32@gmail.com.com>
2026-08-07 15:26:21 +0530

    feat: add the popular dishes utility

---
+++ lib/popular-dishes.ts
@@ -0,0 +1,14 @@
+export const POPULAR_DISHES = [
+  "Chicken Biryani",
+  "Vegetable Biryani",
+  "Chicken Fried Rice",
+  "Egg Fried Rice",
+  "Vegetable Fried Rice",
+  "Chicken Kottu",
+  "Cheese Kottu",
+  "Nasi Goreng",
+  "Chicken Curry",
+  "Fish Curry",
+  "Dhal Curry",
+  "Vegetable Curry",
+] as const;
